19ccef0319f0d3145f16974413589e8759f9b094
Dinh Thoai Tran @ randrise.com <149842088+dinhtt-randrise@users.noreply.github.com>
2024-01-22 21:29:12 -0500

    Update controller.py

--- controller.py
+++ controller.py
@@ -4,11 +4,3 @@ from datetime import datetime
 from . import github as gggh
 from . import logger as gglg
 
-LG_ID = 0
-
-def run_batch_w_logger(batch_no, lid, ALL_LOGGER):
-  global LG_ID
-  gglg.ALL_LOGGER = ALL_LOGGER
-  gglg.save_log(LG_ID, '=> [B] ' + str(batch_no))
-  return ALL_LOGGER
-  
